c15e8d91ea139e503ef6a59062190871f423e3dc
nelson lassa <129919174+NelsonLassa@users.noreply.github.com>
2024-10-15 19:20:17 -0300

    Creacion de proyecto

---
+++ backendtroopers/src/main/java/junior_tech_troopers/backendtroopers/BackendtroopersApplication.java
@@ -0,0 +1,13 @@
+package junior_tech_troopers.backendtroopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtroopersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtroopersApplication.class, args);
+	}
+
+}
---
+++ backendtroopers/src/test/java/junior_tech_troopers/backendtroopers/BackendtroopersApplicationTests.java
@@ -0,0 +1,13 @@
+package junior_tech_troopers.backendtroopers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtroopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
