9162b24bd9ca9fe7b78069c8706e7529ca6f6068
Yamil Jesus Yunes <127790737+yunesyamil@users.noreply.github.com>
2024-11-01 22:14:54 -0300

    DTO iniciales

--- backendtroopers/src/main/java/junior_tech_troopers/backendtroopers/DTO/response/HomeDTO.java
+++ backendtroopers/src/main/java/junior_tech_troopers/backendtroopers/DTO/response/HomeDTO.java
@@ -1,4 +1,18 @@
-package junior_tech_troopers.backendtroopers.DTO.response;
+package prueba1.login.DTO.response;
 
+import lombok.*;
+import prueba1.login.entity.HospedajeImg;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class HomeDTO {
+    private String nombreHospedaje;
+    private String description;
+    private String img_url;
+    private String locality;
 }
